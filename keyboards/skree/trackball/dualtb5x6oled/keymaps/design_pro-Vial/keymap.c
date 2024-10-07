5addaaeb83ad2333e6edc39601f47d2edd061547
Jeron Kuxhausen <jronkuxhausen@gmail.com>
2024-10-06 22:12:49 -0600

    DACTYL - Finished Setting Up the Base of the Dactyl

--- keyboards/skree/trackball/dualtb5x6oled/keymaps/design_pro-Vial/keymap.c
+++ keyboards/skree/trackball/dualtb5x6oled/keymaps/design_pro-Vial/keymap.c
@@ -30,92 +30,92 @@ enum wyld_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_5x6(
-        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,   KC_5,           KC_6,   KC_7,   KC_8,       KC_9,   KC_0,       QK_BOOT,
-        KC_TAB,     KC_Q,   KC_W,   KC_E,       KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_NO,
-        KC_F15,     KC_A,   KC_S,   KC_D,       KC_F,   KC_G,           KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
-        KC_LSFT,    KC_Z,   KC_X,   KC_C,       KC_V,   KC_B,           KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
-                                    KC_BSPC,    KC_SPC, KC_ENT,         KC_ENT, KC_SPC, KC_BSPC,
-                                                KC_NO,  KC_NO,          KC_NO,  KC_NO
+        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,       KC_5,           KC_6,               KC_7,   KC_8,       KC_9,   KC_0,       QK_BOOT,
+        KC_TAB,     KC_Q,   KC_W,   KC_E,       KC_R,       KC_T,           KC_Y,               KC_U,   KC_I,       KC_O,   KC_P,       KC_NO,
+        KC_F15,     KC_A,   KC_S,   KC_D,       KC_F,       KC_G,           KC_H,               KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
+        KC_LSFT,    KC_Z,   KC_X,   KC_C,       KC_V,       KC_B,           KC_N,               KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
+                                    KC_BSPC,    KC_SPC,     KC_ENT,         KC_ENT,             KC_SPC, KC_BSPC,
+                                                KC_BTN2,    KC_BTN1,        OSL(_SELECTOR),     KC_NO
     ),
 
     [_QWERTY] = LAYOUT_5x6(
-        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,   KC_5,           KC_6,   KC_7,   KC_8,       KC_9,   KC_0,       KC_NO,
-        KC_TAB,     KC_Q,   KC_W,   KC_E,       KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_NO,
-        KC_F15,     KC_A,   KC_S,   KC_D,       KC_F,   KC_G,           KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
-        KC_LSFT,    KC_Z,   KC_X,   KC_C,       KC_V,   KC_B,           KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
-                                    KC_BSPC,    KC_SPC, KC_ENT,         KC_ENT, KC_SPC, KC_BSPC,
-                                                KC_NO,  KC_NO,          KC_NO,  KC_NO
+        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,       KC_5,           KC_6,               KC_7,   KC_8,       KC_9,   KC_0,       KC_NO,
+        KC_TAB,     KC_Q,   KC_W,   KC_E,       KC_R,       KC_T,           KC_Y,               KC_U,   KC_I,       KC_O,   KC_P,       KC_NO,
+        KC_F15,     KC_A,   KC_S,   KC_D,       KC_F,       KC_G,           KC_H,               KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
+        KC_LSFT,    KC_Z,   KC_X,   KC_C,       KC_V,       KC_B,           KC_N,               KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
+                                    KC_BSPC,    KC_SPC,     KC_ENT,         KC_ENT,             KC_SPC, KC_BSPC,
+                                                KC_BTN2,    KC_BTN1,        OSL(_SELECTOR),     KC_NO
     ),
 
     [_DVORAK] = LAYOUT_5x6(
-        KC_LBRC,    KC_1,       KC_2,       KC_3,       KC_4,   KC_5,            KC_6,      KC_7,   KC_8,   KC_9,   KC_0,       KC_RBRC,
-        KC_QUOT,    KC_COMM,    KC_DOT,     KC_P,       KC_Y,   KC_F,            KC_G,      KC_C,   KC_R,   KC_L,   KC_SLSH,    KC_EQL,
-        KC_A,       KC_O,       KC_E,       KC_U,       KC_I,   KC_D,            KC_H,      KC_T,   KC_N,   KC_S,   KC_MINS,     KC_NO,
-        KC_LSFT,    KC_SCLN,    KC_Q,       KC_J,       KC_K,   KC_X,            KC_B,      KC_M,   KC_W,   KC_V,   KC_Z,       KC_RSFT,
-                                            KC_BSPC,    KC_SPC, KC_ENT,          KC_ENT,    KC_SPC, KC_BSPC,
-                                                        KC_NO,  KC_NO,           KC_NO,     KC_NO
+        KC_LBRC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,            KC_6,              KC_7,   KC_8,   KC_9,   KC_0,       KC_RBRC,
+        KC_QUOT,    KC_COMM,    KC_DOT,     KC_P,       KC_Y,       KC_F,            KC_G,              KC_C,   KC_R,   KC_L,   KC_SLSH,    KC_EQL,
+        KC_A,       KC_O,       KC_E,       KC_U,       KC_I,       KC_D,            KC_H,              KC_T,   KC_N,   KC_S,   KC_MINS,     KC_NO,
+        KC_LSFT,    KC_SCLN,    KC_Q,       KC_J,       KC_K,       KC_X,            KC_B,              KC_M,   KC_W,   KC_V,   KC_Z,       KC_RSFT,
+                                            KC_BSPC,    KC_SPC,     KC_ENT,          KC_ENT,            KC_SPC, KC_BSPC,
+                                                        KC_BTN2,    KC_BTN1,         OSL(_SELECTOR),    KC_NO
     ),
 
     [_COLEMAK] = LAYOUT_5x6(
-        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,   KC_5,           KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_NO,
-        KC_TAB,     KC_Q,   KC_W,   KC_F,       KC_P,   KC_G,           KC_J,   KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_NO,
-        KC_F15,     KC_A,   KC_R,   KC_S,       KC_T,   KC_D,           KC_H,   KC_N,       KC_E,       KC_I,       KC_O,       KC_NO,
-        KC_LSFT,    KC_Z,   KC_X,   KC_V,       KC_B,   KC_K,           KC_M,   KC_COMM,    KC_DOT,     KC_SLSH,    KC_NO,      KC_RSFT,
-                                    KC_BSPC,    KC_SPC, KC_ENT,         KC_ENT, KC_SPC,     KC_BSPC,
-                                                KC_NO,  KC_NO,          KC_NO,  KC_NO
+        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,       KC_5,           KC_6,               KC_7,       KC_8,       KC_9,       KC_0,       KC_NO,
+        KC_TAB,     KC_Q,   KC_W,   KC_F,       KC_P,       KC_G,           KC_J,               KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_NO,
+        KC_F15,     KC_A,   KC_R,   KC_S,       KC_T,       KC_D,           KC_H,               KC_N,       KC_E,       KC_I,       KC_O,       KC_NO,
+        KC_LSFT,    KC_Z,   KC_X,   KC_V,       KC_B,       KC_K,           KC_M,               KC_COMM,    KC_DOT,     KC_SLSH,    KC_NO,      KC_RSFT,
+                                    KC_BSPC,    KC_SPC,     KC_ENT,         KC_ENT,             KC_SPC,     KC_BSPC,
+                                                KC_BTN2,    KC_BTN1,        OSL(_SELECTOR),     KC_NO
     ),
 
     [_ILLUSTRATOR] = LAYOUT_5x6(
-        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,   KC_5,           KC_6,   KC_7,   KC_8,       KC_9,   KC_0,       QK_BOOT,
-        KC_TAB,     KC_Q,   KC_W,   KC_E,       KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_NO,
-        KC_F15,     KC_A,   KC_S,   KC_D,       KC_F,   KC_G,           KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
-        KC_LSFT,    KC_Z,   KC_X,   KC_C,       KC_V,   KC_B,           KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
-                                    KC_BSPC,    KC_SPC, KC_ENT,         KC_ENT, KC_SPC, KC_BSPC,
-                                                KC_NO,  KC_NO,          KC_NO,  KC_NO
+        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,       KC_5,           KC_6,               KC_7,   KC_8,       KC_9,   KC_0,       KC_NO,
+        KC_TAB,     KC_Q,   KC_W,   KC_E,       KC_R,       KC_T,           KC_Y,               KC_U,   KC_I,       KC_O,   KC_P,       KC_NO,
+        KC_F15,     KC_A,   KC_S,   KC_D,       KC_F,       KC_G,           KC_H,               KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
+        KC_LSFT,    KC_Z,   KC_X,   KC_C,       KC_V,       KC_B,           KC_N,               KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
+                                    KC_BSPC,    KC_SPC,     KC_ENT,         KC_ENT,             KC_SPC, KC_BSPC,
+                                                KC_BTN2,    KC_BTN1,        OSL(_SELECTOR),     KC_NO
     ),
 
     [_PHOTOSHOP] = LAYOUT_5x6(
-        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,   KC_5,           KC_6,   KC_7,   KC_8,       KC_9,   KC_0,       QK_BOOT,
-        KC_TAB,     KC_Q,   KC_W,   KC_E,       KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_NO,
-        KC_F15,     KC_A,   KC_S,   KC_D,       KC_F,   KC_G,           KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
-        KC_LSFT,    KC_Z,   KC_X,   KC_C,       KC_V,   KC_B,           KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
-                                    KC_BSPC,    KC_SPC, KC_ENT,         KC_ENT, KC_SPC, KC_BSPC,
-                                                KC_NO,  KC_NO,          KC_NO,  KC_NO
+        KC_ESC,     KC_1,   KC_2,   KC_3,       KC_4,       KC_5,           KC_6,               KC_7,   KC_8,       KC_9,   KC_0,       KC_NO,
+        KC_TAB,     KC_Q,   KC_W,   KC_E,       KC_R,       KC_T,           KC_Y,               KC_U,   KC_I,       KC_O,   KC_P,       KC_NO,
+        KC_F15,     KC_A,   KC_S,   KC_D,       KC_F,       KC_G,           KC_H,               KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,
+        KC_LSFT,    KC_Z,   KC_X,   KC_C,       KC_V,       KC_B,           KC_N,               KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
+                                    KC_BSPC,    KC_SPC,     KC_ENT,         KC_ENT,             KC_SPC, KC_BSPC,
+                                                KC_BTN2,    KC_BTN1,        OSL(_SELECTOR),     KC_NO
     ),
 
     [_SWITCH] = LAYOUT_5x6(
-        KC_ESC,     KC_6,   KC_7,   KC_8,       KC_9,   KC_0,           KC_1,   KC_2,   KC_3,       KC_4,   KC_5,   QK_BOOT,
-        KC_TAB,     KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,           KC_Q,   KC_W,   KC_E,       KC_R,   KC_T,   KC_NO,
-        KC_F15,     KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,        KC_A,   KC_S,   KC_D,       KC_F,   KC_G,   KC_QUOT,
-        KC_LSFT,    KC_Z,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,        KC_Z,   KC_X,   KC_C,       KC_V,   KC_B,   KC_RSFT,
-                                    KC_BSPC,    KC_SPC, KC_ENT,         KC_ENT, KC_SPC, KC_BSPC,
-                                                KC_NO,  KC_NO,          KC_NO,  KC_NO
+        KC_ESC,     KC_6,   KC_7,   KC_8,       KC_9,       KC_0,           KC_1,               KC_2,   KC_3,       KC_4,   KC_5,   KC_NO,
+        KC_TAB,     KC_Y,   KC_U,   KC_I,       KC_O,       KC_P,           KC_Q,               KC_W,   KC_E,       KC_R,   KC_T,   KC_NO,
+        KC_F15,     KC_H,   KC_J,   KC_K,       KC_L,       KC_SCLN,        KC_A,               KC_S,   KC_D,       KC_F,   KC_G,   KC_QUOT,
+        KC_LSFT,    KC_Z,   KC_M,   KC_COMM,    KC_DOT,     KC_SLSH,        KC_Z,               KC_X,   KC_C,       KC_V,   KC_B,   KC_RSFT,
+                                    KC_BSPC,    KC_SPC,     KC_ENT,         KC_ENT,             KC_SPC, KC_BSPC,
+                                                KC_BTN2,    KC_BTN1,        OSL(_SELECTOR),     KC_NO
     ),
 
     [_FUNCPAD] = LAYOUT_5x6(
-        KC_ESC,     KC_F1,  KC_F2,      KC_F3,      KC_F4,      KC_F5,          KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-        KC_TAB,     KC_F6,  KC_F7,      KC_F8,      KC_F9,      KC_F10,         KC_NO,  KC_7,   KC_8,   KC_9,   KC_NO,  KC_NO,
-        KC_F15,     KC_F11, KC_NO,      KC_UP,      KC_NO,      KC_F12,         KC_NO,  KC_4,   KC_5,   KC_6,   KC_NO,  KC_NO,
-        KC_LSFT,    KC_NO,  KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_NO,          KC_NO,  KC_1,   KC_2,   KC_3,   KC_NO,  KC_NO,
-                                        KC_BSPC,    KC_SPC,     KC_ENT,         KC_ENT, KC_0,   KC_DOT,
-                                                    KC_NO,      KC_NO,          KC_NO,  KC_NO
+        KC_ESC,     KC_F1,  KC_F2,      KC_F3,      KC_F4,      KC_F5,          KC_NO,              KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+        KC_TAB,     KC_F6,  KC_F7,      KC_F8,      KC_F9,      KC_F10,         KC_NO,              KC_7,   KC_8,   KC_9,   KC_NO,  KC_NO,
+        KC_F15,     KC_F11, KC_NO,      KC_UP,      KC_NO,      KC_F12,         KC_NO,              KC_4,   KC_5,   KC_6,   KC_NO,  KC_NO,
+        KC_LSFT,    KC_NO,  KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_NO,          KC_NO,              KC_1,   KC_2,   KC_3,   KC_NO,  KC_NO,
+                                        KC_BSPC,    KC_SPC,     KC_ENT,         KC_ENT,             KC_0,   KC_DOT,
+                                                    KC_NO,      KC_NO,          OSL(_SELECTOR),     KC_NO
     ),
 
     [_BRACKETS] = LAYOUT_5x6(
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-                                _______,_______,_______,            _______,_______,_______,
-                                        _______,_______,            _______,_______
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,            KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+                                KC_NO,  KC_NO,  KC_NO,            KC_NO,    KC_NO,  KC_NO,
+                                        KC_NO,  KC_NO,            KC_NO,    KC_NO
     ),
 
     [_SELECTOR] = LAYOUT_5x6(
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-                                _______,_______,_______,            _______,_______,_______,
+        _______,_______,_______,_______,_______,_______,            _______,_______,            _______,            _______,        _______,_______,
+        _______,_______,_______,_______,_______,_______,            _______,TO(_RGB),           TO(_MEDIA),         TO(_MOUSE),     _______,_______,
+        _______,_______,_______,_______,_______,_______,            _______,TO(_ILLUSTRATOR),   TO(_PHOTOSHOP),     TO(_SWITCH),    _______,_______,
+        _______,_______,_______,_______,_______,_______,            _______,TO(_QWERTY),        TO(_DVORAK),        TO(_COLEMAK),   _______,_______,
+                                _______,_______,_______,            _______,TO(_BASE),          _______,
                                         _______,_______,            _______,_______
     ),
 
@@ -138,12 +138,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_MOUSE] = LAYOUT_5x6(
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
-                                _______,_______,_______,            _______,_______,_______,
-                                        _______,_______,            _______,_______
+        _______,_______,_______,_______,_______,_______,            _______,    _______,    _______,    _______,    _______,_______,
+        _______,_______,_______,_______,_______,_______,            _______,    _______,    _______,    _______,    _______,_______,
+        _______,_______,_______,_______,_______,_______,            _______,    KC_BTN1,    KC_BTN2,    KC_BTN3,    _______,_______,
+        _______,_______,_______,_______,_______,_______,            _______,    _______,    _______,    _______,    _______,_______,
+                                _______,_______,_______,            _______,    _______,    _______,
+                                        _______,_______,            _______,    _______
         )
 };
 
@@ -328,15 +328,6 @@ bool oled_task_user(void) {
 }
 #endif
 
-#if defined(ENCODER_MAP_ENABLE)
-// TWO ENCODERS
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [_QWERTY] =  { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_LOWER] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-    [_RAISE] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN) },
-    [_MOUSE] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
-};
-#endif
 
 
 
